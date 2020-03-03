364b8edbd33aea3f3ae31c4b83031b423a3d667b
iamdarkes <josephdarkes@gmail.com>
2020-03-03 01:22:01 -0800

    Updates

---
+++ precache-manifest.6c6f09bed8b07f9f8eb0acc2a7ff6ad8.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6831246143f9a78f22216ee13fc48ca8",
+    "url": "/victory-road/index.html"
+  },
+  {
+    "revision": "1de2b3c76bedbe85b54b",
+    "url": "/victory-road/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fbb4809788da05851958",
+    "url": "/victory-road/static/js/2.16edfb95.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/victory-road/static/js/2.16edfb95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de2b3c76bedbe85b54b",
+    "url": "/victory-road/static/js/main.3eb72aef.chunk.js"
+  },
+  {
+    "revision": "61769c350d5a052df856",
+    "url": "/victory-road/static/js/runtime-main.dcee4476.js"
+  },
+  {
+    "revision": "6575259722124c4a318ad6485f2078fd",
+    "url": "/victory-road/static/media/10.65752597.png"
+  },
+  {
+    "revision": "f1d4449ea4b409ecce6b7cda5be193ba",
+    "url": "/victory-road/static/media/112.f1d4449e.png"
+  },
+  {
+    "revision": "6fde2b0d5f05b342b44c382d9a65b09c",
+    "url": "/victory-road/static/media/142.6fde2b0d.png"
+  },
+  {
+    "revision": "c7476a0269a02b549000faabb6ad7569",
+    "url": "/victory-road/static/media/143.c7476a02.png"
+  },
+  {
+    "revision": "3770c7145c3f756f848c24aee7ece874",
+    "url": "/victory-road/static/media/150.3770c714.png"
+  },
+  {
+    "revision": "370a65ee36386b00be2157d1d74d795b",
+    "url": "/victory-road/static/media/197.370a65ee.png"
+  },
+  {
+    "revision": "eaba4eebec35597f8cc004b0c2e2e458",
+    "url": "/victory-road/static/media/206.eaba4eeb.png"
+  },
+  {
+    "revision": "e31302e73e8a73521fcc24d9195e54ce",
+    "url": "/victory-road/static/media/212.e31302e7.png"
+  },
+  {
+    "revision": "2e9f294e7ad9f510b1599dba11e11306",
+    "url": "/victory-road/static/media/213.2e9f294e.png"
+  },
+  {
+    "revision": "52fda7c9ddebce529fdb2f75e078e4f5",
+    "url": "/victory-road/static/media/214.52fda7c9.png"
+  },
+  {
+    "revision": "67211dde1faed21c56939ea75429bb7a",
+    "url": "/victory-road/static/media/248.67211dde.png"
+  },
+  {
+    "revision": "8ed528dfb3524121804b257bc81c2fe3",
+    "url": "/victory-road/static/media/260.8ed528df.png"
+  },
+  {
+    "revision": "fbf08e5dfec08e1aa96114f45387476a",
+    "url": "/victory-road/static/media/280.fbf08e5d.png"
+  },
+  {
+    "revision": "1bce83699981ac87ddfb4d6d9e3f12f7",
+    "url": "/victory-road/static/media/286.1bce8369.png"
+  },
+  {
+    "revision": "cb0c824e49308e8e96a1f58940f96ae2",
+    "url": "/victory-road/static/media/287.cb0c824e.png"
+  },
+  {
+    "revision": "6f939c448b05000072270443f5f6711a",
+    "url": "/victory-road/static/media/335.6f939c44.png"
+  },
+  {
+    "revision": "4b59776f2cd58899f4ea5f24a0c708cd",
+    "url": "/victory-road/static/media/349.4b59776f.png"
+  },
+  {
+    "revision": "fce3d1d3ee81d8ff1382df4216da2dc4",
+    "url": "/victory-road/static/media/356.fce3d1d3.png"
+  },
+  {
+    "revision": "33ebd9bb96186333f6b81c4746ff90d9",
+    "url": "/victory-road/static/media/373.33ebd9bb.png"
+  },
+  {
+    "revision": "06bb027c72b2de4936c31f13371e1707",
+    "url": "/victory-road/static/media/376.06bb027c.png"
+  },
+  {
+    "revision": "908ffd886bdc99e2a5a87e0b5c5ed3e2",
+    "url": "/victory-road/static/media/399.908ffd88.png"
+  },
+  {
+    "revision": "775cd32a74a798e8a0700f3c7e34a956",
+    "url": "/victory-road/static/media/4.775cd32a.png"
+  },
+  {
+    "revision": "d497c96f0c20a22d1fac0e102c7b6c8f",
+    "url": "/victory-road/static/media/404.d497c96f.png"
+  },
+  {
+    "revision": "054486d92713a1dc96f02d938200c49e",
+    "url": "/victory-road/static/media/409.054486d9.png"
+  },
+  {
+    "revision": "d4b78cccd79b3216c8ec88297025b0bc",
+    "url": "/victory-road/static/media/418.d4b78ccc.png"
+  },
+  {
+    "revision": "9f9129e19eb24ed5ffa60d9ad5ea1ac9",
+    "url": "/victory-road/static/media/422-west.9f9129e1.png"
+  },
+  {
+    "revision": "899e7b770e65b83e2a6f66f1cb44b90b",
+    "url": "/victory-road/static/media/426.899e7b77.png"
+  },
+  {
+    "revision": "f3a86e75e54912f2bd45218b4f9a705e",
+    "url": "/victory-road/static/media/441.f3a86e75.png"
+  },
+  {
+    "revision": "a84fc25a6ea506477dbe6829e81d6982",
+    "url": "/victory-road/static/media/445.a84fc25a.png"
+  },
+  {
+    "revision": "6d8381c9929ef9453111894766e2a36f",
+    "url": "/victory-road/static/media/448.6d8381c9.png"
+  },
+  {
+    "revision": "3a74a8009d2ad7f20271693f5d88c493",
+    "url": "/victory-road/static/media/50.3a74a800.png"
+  },
+  {
+    "revision": "7587807f994eb1d5efb982e143b7f2fe",
+    "url": "/victory-road/static/media/530.7587807f.png"
+  },
+  {
+    "revision": "832e323433252383282d90a695fa6ed9",
+    "url": "/victory-road/static/media/545.832e3234.png"
+  },
+  {
+    "revision": "ad02914b3acb56dd5962cda05bdc2ebb",
+    "url": "/victory-road/static/media/559.ad02914b.png"
+  },
+  {
+    "revision": "576e05b24e6a3fdf2d9aa2e814a85b61",
+    "url": "/victory-road/static/media/568.576e05b2.png"
+  },
+  {
+    "revision": "18f9d16beaee44e54e7479a4fcb32d6c",
+    "url": "/victory-road/static/media/609.18f9d16b.png"
+  },
+  {
+    "revision": "5527db5b492dd29de6774a8834fa970a",
+    "url": "/victory-road/static/media/61.5527db5b.png"
+  },
+  {
+    "revision": "c80509cea89f87cfc00e6c96c6e11bac",
+    "url": "/victory-road/static/media/68.c80509ce.png"
+  },
+  {
+    "revision": "f8d63c71f63274eb9cd438bda0d812cd",
+    "url": "/victory-road/static/media/79.f8d63c71.png"
+  },
+  {
+    "revision": "d13b62e1e12901fe153097cff5773e24",
+    "url": "/victory-road/static/media/93.d13b62e1.png"
+  }
+]);
